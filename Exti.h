f9d51295df04ba0221ebbfa418e7d3e80ea93184
BSP Release <release@bsp>
2026-04-20 15:44:49 +0000

    Release v0.0.1 for STM32U5

---
+++ Exti.h
@@ -0,0 +1,36 @@
+/**
+ * \author Mr.Nobody
+ * \file Exti.h
+ * \ingroup Exti
+ * \brief Exti module common functionality header file.
+ *
+ * This file contains the common functionality used internally by the module,
+ * and shall provide interface between the module and the application.
+ *
+ */
+
+#ifndef EXTI_EXTI_H
+#define EXTI_EXTI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* ============================= INCLUDES =================================== */
+#include "Exti_Types.h"                     /* Module types definition        */
+/* ============================= TYPEDEFS =================================== */
+
+/* ========================= SYMBOLIC CONSTANTS ============================= */
+
+/* ========================= EXPORTED MACROS ================================ */
+
+/* ========================= EXPORTED VARIABLES ============================= */
+
+/* ======================== EXPORTED FUNCTIONS ============================== */
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* EXTI_EXTI_H */
